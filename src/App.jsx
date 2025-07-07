fdd1a9368110002ce9eabcfb55825ced51d84e08
Satyam Rajput <satyamrajput081998@gmail.com>
2025-07-07 04:27:07 -0400

    add a input Filed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Button from './Basic Components/Button/button'
+import Input from './Basic Components/input/input'
 function App() {
   return (
     <>
      <h3>Welcome to React</h3>
      <Button>{"click me"}</Button>
+     <Input />
     </>
   )
 }
---
+++ src/Basic Components/input/input.jsx
@@ -0,0 +1,8 @@
+const Input = ({onchange,className}) => {
+    return(
+        <>
+        <input onChange={onchange} className={className}/>
+        </>
+    )
+}
+export default Input
