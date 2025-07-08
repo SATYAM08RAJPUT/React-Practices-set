0fd1242e350e781d34b5e820eab240d8e81adca6
Satyam Rajput <satyamrajput081998@gmail.com>
2025-07-08 04:06:11 -0400

    applied some CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Button from './Basic Components/Button/button'
+import Form from './Basic Components/Form/form'
 import Input from './Basic Components/input/input'
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
      <h3>Welcome to React</h3>
      <Button>{"click me"}</Button>
      <Input placeholder={"search a Name"} className={"inputfield"} />
-   
+    <Form />
     </>
   )
 }
