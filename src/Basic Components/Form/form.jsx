5e93c1e3aacfb0f7f84c33986ade4c72e34c26df
Satyam Rajput <satyamrajput081998@gmail.com>
2025-07-08 04:02:43 -0400

    make a Form Component

---
+++ src/Basic Components/Form/form.jsx
@@ -0,0 +1,18 @@
+const Form = () => {
+    return(
+        <>
+        <form>
+                <h2>User Form</h2>
+                <label>
+                    Name:
+                    <input type="text" required/>
+                </label>
+                <label>
+                    Email:
+                    <input type="text" required/>
+                </label>
+        </form>
+        </>
+    )
+}
+export default Form
